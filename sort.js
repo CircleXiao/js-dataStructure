c5af5302848328910fc73b3ed56bb653058f8582
mmy <mmy_xize@163.com>
2019-03-22 14:44:30 +0800

    the sort.js add selectionSort  function

--- sort.js
+++ sort.js
@@ -21,7 +21,11 @@
         array[index2] = aux;
     };
 
-    // 冒泡排序：在所有排序算法中最简单，但运行时间上看最差O(n*n)
+    /**
+     * 冒泡排序：所以排序算法中最简单，但性能最差
+     * 算法思想：比较任意两个相邻的项，如果第一个比第二个大，则交换它们。向上移动至正确的顺序
+     * 时间复杂度：O(n²)
+     */
     this.bubbleSort = function () {
         var length = array.length;
         for (var i=0; i<length; i++) {
@@ -45,6 +49,28 @@
         }
     };
 
+    /**
+     * 选择排序
+     * 算法思想：找到数据结构中的最小值并将其放置在第一位，接着找到第二小的值并放在第二位，以此类推
+     * 时间复杂度：O(n²)
+    */
+    this.selectionSort = function () {
+        var length = array.length,
+        indexMin;
+        for (var i=0; i<length-1; i++) {
+            indexMin = i;
+            for (var j=i; j<length; j++) {
+                if (array[indexMin] > array[j]) {
+                    indexMin = j;
+                }
+            }
+            if (i !== indexMin) {
+                swap(array, i, indexMin);
+            }
+        }
+
+    };
+
 
 };
 
@@ -61,11 +87,11 @@ function createNonSortedArray (size) {
 
 var array = createNonSortedArray(5);
 console.log(array.toString());
-array.bubbleSort();
+
+// array.bubbleSort();
+// array.modifiedBullbleSort();
+array.selectionSort();
+
 console.log(array.toString());
 
-var array2 = createNonSortedArray(6);
-console.log(array2.toString());
-array2.modifiedBullbleSort();
-console.log(array2.toString());
 
